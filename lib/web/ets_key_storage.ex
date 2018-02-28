6e145217170d6e296e68f78878b473d72268cfdf
Ellis Pritchard <ellis.pritchard@ft.com>
2018-02-28 12:07:41 +0000

    rename some references to key 'tags'

--- lib/web/ets_key_storage.ex
+++ lib/web/ets_key_storage.ex
@@ -12,30 +12,30 @@ defmodule FT.Web.ETSKeyStorage do
   def setup("" <> keys) do
     table_ref = :ets.new(__MODULE__, [:named_table, :protected, {:read_concurrency, true}])
     keys = expand_keys(keys)
-    Enum.each(keys, fn key_tags -> :ets.insert(table_ref, key_tags) end)
+    Enum.each(keys, fn key_roles -> :ets.insert(table_ref, key_roles) end)
   end
 
   @spec lookup(key :: String.t) :: {:ok, FT.Web.KeyStorage.roles} | false
   @impl FT.Web.KeyStorage
   def lookup(key) do
     case :ets.lookup(__MODULE__, key) do
-      [{_key, tags}] -> {:ok, tags}
+      [{_key, roles}] -> {:ok, roles}
       [] -> false
     end
   end
 
-  # split key config string into a map of `key => [tag, ...]`
+  # split key config string into a map of `"key" => %{:role => true, ...}`
   defp expand_keys("" <> keys) do
     keys # "key1,key2<>x,key3<>x<>y"
     |> String.splitter(",", trim: true) # ["key1", "key2<>x", "key3<>x<>y"]
     |> Stream.map(&(String.split(&1, "<>", trim: true))) #  [["key1"], ["key2", "x"], ["key3", "x", "y"]
-    |> Stream.map(fn [key | tags] -> {key, tags} end) # [{"key1", []}, {"key2", ["x"]}, ...]
-    |> Stream.map(fn {key, tags} -> {key, to_roles(tags)} end) # [{"key1", %{}}, {"key2", %{x: true}}, ...]
+    |> Stream.map(fn [key | roles] -> {key, roles} end) # [{"key1", []}, {"key2", ["x"]}, ...]
+    |> Stream.map(fn {key, roles} -> {key, to_roles_map(roles)} end) # [{"key1", %{}}, {"key2", %{x: true}}, ...]
     |> Enum.into(%{})
   end
 
-  defp to_roles(tags) when is_list(tags) do
-    Enum.into(tags, %{}, fn
+  defp to_roles_map(roles) when is_list(roles) do
+    Enum.into(roles, %{}, fn
       tag when is_atom(tag) -> {tag, true}
       tag -> {String.to_atom(tag), true}
     end)
